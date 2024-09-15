62c0048b0826bc110ed49795cd4d087f455cc8f5
Amigo <8450881+AmigoSt@users.noreply.github.com>
2024-09-16 02:32:52 +0300

    2024-09-16
    +fix project structure
    +separate to modules
    +add animation to grid

---
+++ js/M_gride_v1.js
@@ -0,0 +1,121 @@
+//let {f_M_gride_v1} = () => {
+export function f_M_gride_v1() {
+//function f_M_gride_v1() {
+
+    var M_gride_v1 = new BABYLON.NodeMaterial("M_gride_v1");
+    M_gride_v1.mode = BABYLON.NodeMaterialModes.Material;
+
+    // InputBlock
+    var position = new BABYLON.InputBlock("position");
+    position.visibleInInspector = false;
+    position.visibleOnFrame = false;
+    position.target = 1;
+    position.setAsAttribute("position");
+
+    // TransformBlock
+    var WorldPos = new BABYLON.TransformBlock("WorldPos");
+    WorldPos.visibleInInspector = false;
+    WorldPos.visibleOnFrame = false;
+    WorldPos.target = 1;
+    WorldPos.complementZ = 0;
+    WorldPos.complementW = 1;
+
+    // InputBlock
+    var World = new BABYLON.InputBlock("World");
+    World.visibleInInspector = false;
+    World.visibleOnFrame = false;
+    World.target = 1;
+    World.setAsSystemValue(BABYLON.NodeMaterialSystemValues.World);
+
+    // TransformBlock
+    var WorldPosViewProjectionTransform = new BABYLON.TransformBlock("WorldPos * ViewProjectionTransform");
+    WorldPosViewProjectionTransform.visibleInInspector = false;
+    WorldPosViewProjectionTransform.visibleOnFrame = false;
+    WorldPosViewProjectionTransform.target = 1;
+    WorldPosViewProjectionTransform.complementZ = 0;
+    WorldPosViewProjectionTransform.complementW = 1;
+
+    // InputBlock
+    var ViewProjection = new BABYLON.InputBlock("ViewProjection");
+    ViewProjection.visibleInInspector = false;
+    ViewProjection.visibleOnFrame = false;
+    ViewProjection.target = 1;
+    ViewProjection.setAsSystemValue(BABYLON.NodeMaterialSystemValues.ViewProjection);
+
+    // VertexOutputBlock
+    var VertexOutput = new BABYLON.VertexOutputBlock("VertexOutput");
+    VertexOutput.visibleInInspector = false;
+    VertexOutput.visibleOnFrame = false;
+    VertexOutput.target = 1;
+
+    // InputBlock
+    var uv = new BABYLON.InputBlock("uv");
+    uv.visibleInInspector = false;
+    uv.visibleOnFrame = false;
+    uv.target = 1;
+    uv.setAsAttribute("uv");
+
+    // TextureBlock
+    var Texture = new BABYLON.TextureBlock("Texture");
+    Texture.visibleInInspector = false;
+    Texture.visibleOnFrame = false;
+    Texture.target = 3;
+    Texture.convertToGammaSpace = false;
+    Texture.convertToLinearSpace = false;
+    Texture.disableLevelMultiplication = false;
+    Texture.texture = new BABYLON.Texture("https://raw.githubusercontent.com/AmigoSt/project-s/main/assets/textures/t_plane_v1.png", null, false, false, 3);
+    Texture.texture.wrapU = 1;
+    Texture.texture.wrapV = 1;
+    Texture.texture.uAng = 0;
+    Texture.texture.vAng = 0;
+    Texture.texture.wAng = 0;
+    Texture.texture.uOffset = 0;
+    Texture.texture.vOffset = 0;
+    Texture.texture.uScale = 1;
+    Texture.texture.vScale = 1;
+    Texture.texture.coordinatesMode = 7;
+
+    // ImageSourceBlock
+    var ImageSource = new BABYLON.ImageSourceBlock("ImageSource");
+    ImageSource.visibleInInspector = false;
+    ImageSource.visibleOnFrame = false;
+    ImageSource.target = 3;
+    ImageSource.texture = new BABYLON.Texture("https://raw.githubusercontent.com/AmigoSt/project-s/main/assets/textures/t_plane_v1.png", null, false, false, 3);
+    ImageSource.texture.wrapU = 1;
+    ImageSource.texture.wrapV = 1;
+    ImageSource.texture.uAng = 0;
+    ImageSource.texture.vAng = 0;
+    ImageSource.texture.wAng = 0;
+    ImageSource.texture.uOffset = 0;
+    ImageSource.texture.vOffset = 0;
+    ImageSource.texture.uScale = 1;
+    ImageSource.texture.vScale = 1;
+    ImageSource.texture.coordinatesMode = 7;
+
+    // FragmentOutputBlock
+    var FragmentOutput = new BABYLON.FragmentOutputBlock("FragmentOutput");
+    FragmentOutput.visibleInInspector = false;
+    FragmentOutput.visibleOnFrame = false;
+    FragmentOutput.target = 2;
+    FragmentOutput.convertToGammaSpace = false;
+    FragmentOutput.convertToLinearSpace = false;
+    FragmentOutput.useLogarithmicDepth = false;
+
+    // Connections
+    position.output.connectTo(WorldPos.vector);
+    World.output.connectTo(WorldPos.transform);
+    WorldPos.output.connectTo(WorldPosViewProjectionTransform.vector);
+    ViewProjection.output.connectTo(WorldPosViewProjectionTransform.transform);
+    WorldPosViewProjectionTransform.output.connectTo(VertexOutput.vector);
+    uv.output.connectTo(Texture.uv);
+    ImageSource.source.connectTo(Texture.source);
+    Texture.rgba.connectTo(FragmentOutput.rgba);
+
+    // Output nodes
+    M_gride_v1.addOutputNode(VertexOutput);
+    M_gride_v1.addOutputNode(FragmentOutput);
+    M_gride_v1.build();
+    window.M_gride_v1 = M_gride_v1;
+}
+
+//export default f_M_gride_v1;
---
+++ js/projectS.js
@@ -0,0 +1,93 @@
+import {f_M_gride_v1} from './M_gride_v1.js';
+import {f_M_back_v1} from './M_back_v1.js';
+
+var canvas = document.getElementById("renderCanvas");
+
+var startRenderLoop = function (engine, canvas) {
+    engine.runRenderLoop(function () {
+        if (sceneToRender && sceneToRender.activeCamera) {
+            sceneToRender.render();
+        }
+    });
+}
+
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
+let grid_animations_v1 = await BABYLON.Animation.ParseFromFileAsync(null, "./animations/anim_grid_v1.json");
+
+
+
+var createScene = function () {
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+
+    // This creates and positions an arc rotate camera (non-mesh)
+    var camera = new BABYLON.ArcRotateCamera("Camera", BABYLON.Tools.ToRadians(-90), BABYLON.Tools.ToRadians(90), 10, new BABYLON.Vector3(0, 0, 0), scene);    
+    camera.attachControl(canvas, false);
+
+    //camera 2
+    var camera_main = new BABYLON.ArcRotateCamera("Camera_Main", BABYLON.Tools.ToRadians(0), BABYLON.Tools.ToRadians(0), 5, new BABYLON.Vector3(0, 0, 0), scene);    
+    camera_main.attachControl(canvas, false);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+
+    //Place Material Here!!!!
+    
+    f_M_gride_v1();
+    f_M_back_v1();
+
+    
+    //// adding object
+    var box = BABYLON.MeshBuilder.CreateBox("box", {width: 30, height: 20, depth: 0.1 }, scene);
+    box.position.z = 20;
+    box.rotation.y = BABYLON.Tools.ToRadians(180);
+    box.material = M_back_v1;
+
+    BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/AmigoSt/project-s/main/assets/models/", "sphere_v2.glb", scene).then((result) => {
+    const mesh_back = scene.getMeshByName("back");
+    const mesh_main_grid = scene.getMeshByName("main_grid");
+    mesh_back.material = M_gride_v1;
+    mesh_main_grid.material = M_gride_v1;
+
+    // Grid Animation
+    const mesh_grid = scene.getMeshByName("__root__");
+    mesh_grid.position.y =10;
+    mesh_grid.animations = grid_animations_v1;
+    scene.beginAnimation(mesh_grid, 0, 100, true);
+    });
+
+    scene.debugLayer.show();
+
+    return scene;
+};
+
+        window.initFunction = async function() {
+            var asyncEngineCreation = async function() {
+                try {
+                return createDefaultEngine();
+                } catch(e) {
+                console.log("the available createEngine function failed. Creating the default engine instead");
+                return createDefaultEngine();
+                }
+            }
+
+            engine = await asyncEngineCreation();
+    window.engine = engine; 
+if (!engine) throw 'engine should not be null.';
+startRenderLoop(engine, canvas);
+scene = createScene();
+    window.scene = scene;};
+initFunction().then(() => {sceneToRender = scene                    
+});
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
