8cad4ebc75ad97bcb2e09ebf0dd6ba1bc8899f58
Amigo <8450881+AmigoSt@users.noreply.github.com>
2024-11-18 22:42:28 +0300

    dev panel on

--- js/projectS.js
+++ js/projectS.js
@@ -45,6 +45,8 @@ var createScene = function () {
     let loadedGUI = advancedTexture.parseFromURLAsync("./gui/gui_dev_v1.json");
     let btn_devPanel = advancedTexture.getControlByName("btn_devPanels");
     
+    scene.debugLayer.show();
+
     if (btn_devPanel != null) {
         btn_devPanel.onPointerClickObservable.add( () => {  
             scene.debugLayer.show();
