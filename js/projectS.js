3aed006b7c7bce32820bb74f56c2f981811a9b6d
Amigo <8450881+AmigoSt@users.noreply.github.com>
2024-11-23 14:12:32 +0300

    GUI
    * Fixin render scale v1

--- js/projectS.js
+++ js/projectS.js
@@ -43,6 +43,7 @@ var createScene = function () {
     // Load a GUI from a URL JSON.
     let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("GUI", true, scene);
     let loadedGUI = advancedTexture.parseFromURLAsync("./gui/guiTexture.json");
+    BABYLON.GUI.advancedTexture.renderScale = 1.8;
     let btn_devPanel = advancedTexture.getControlByName("btn_dev");
     
 
